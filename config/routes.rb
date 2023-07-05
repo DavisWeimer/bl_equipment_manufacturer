46ed01b8486e0ef25ca8cf8a36c3ab6818e19a38
Davis Weimer <davisweimer@gmail.com>
2023-07-05 17:34:04 -0600

    feat: manufacturers#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/manufacturers', to: 'manufacturers#index'
+  get '/weapons', to: 'weapons#index'
 end
