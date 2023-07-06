6577860561cf70f2bde3ea89269a923692aba0cb
Davis Weimer <davisweimer@gmail.com>
2023-07-06 11:41:33 -0600

    commit before revert

---
+++ db/migrate/20230706001120_add_manufacturer_to_weapons.rb
@@ -0,0 +1,5 @@
+class AddManufacturerToWeapons < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :weapons, :manufacturer, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_195120) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_001120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,30 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_195120) do
     t.integer "num_of_games_appeared_in"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "weapon_id"
+    t.index ["weapon_id"], name: "index_manufacturers_on_weapon_id"
   end
 
+  create_table "weapons", force: :cascade do |t|
+    t.integer "level_req"
+    t.string "rarity"
+    t.string "type"
+    t.string "name"
+    t.integer "damage"
+    t.float "accuracy"
+    t.float "fire_rate"
+    t.float "reload_speed"
+    t.integer "magazine_size"
+    t.boolean "elemental?"
+    t.string "elemental_type"
+    t.string "trait"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "manufacturer_id"
+    t.index ["manufacturer_id"], name: "index_weapons_on_manufacturer_id"
+  end
+
+  add_foreign_key "manufacturers", "weapons"
+  add_foreign_key "weapons", "manufacturers"
 end
