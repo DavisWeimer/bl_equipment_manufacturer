c5f94dd6c522f3ff719ac8a2a981a4c456e5be50
Davis Weimer <davisweimer@gmail.com>
2023-07-07 16:22:10 -0600

    fix: Migrate objects to rails_helper.rb

---
+++ spec/features/weapon/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+=begin
+[✔️] done
+
+User Story 3, Child Index 
+
+As a visitor
+When I visit '/child_table_name'
+Then I see each Child in the system including the Child's attributes
+(data from each column that is on the child table)
+=end
+RSpec.describe 'The weapons index page' do
+  it 'displays the names of all the weapons' do
+    
+  end
+end
