b99ee3569e542c9db065d63030677734c03d5e23
Davis Weimer <davisweimer@gmail.com>
2023-07-07 15:54:38 -0600

    test: manufacturers#index

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -8,7 +8,6 @@ class ManufacturersController < ApplicationController
   end
 
   def show_weapons
-    # require 'pry'; binding.pry
     @manufacturer = Manufacturer.find(params[:id])
     @weapons_list = @manufacturer.weapons
   end
