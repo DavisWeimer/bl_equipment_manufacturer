e4313cde43c6d05595d3bfbce63b8e685125e6f0
Davis Weimer <davisweimer@gmail.com>
2023-07-08 14:53:05 -0600

    feat: @manufacturer.weapons.count

--- spec/features/manufacturer/show_spec.rb
+++ spec/features/manufacturer/show_spec.rb
@@ -8,16 +8,38 @@ As a visitor
 When I visit '/parents/:id'
 Then I see the parent with that id including the parent's attributes
 (data from each column that is on the parent table)
-=end
-RSpec.describe 'The manufacturers show page' do
-  it 'displays the manufacturer by :id and its attributes' do
 
-    visit "/manufacturers/#{@jakobs.id}"
-    
-    expect(page).to have_content(@jakobs.name)
-    expect(page).to have_content(@jakobs.headquarters)
-    expect(page).to have_content(@jakobs.elemental?)
-    expect(page).to have_content(@jakobs.equipment_production_total)
-    expect(page).to_not have_content(@torgue.headquarters)
+[🪄] done
+
+User Story 7, Parent Child Count
+
+As a visitor
+When I visit a parent's show page
+I see a count of the number of children associated with this parent
+=end
+
+RSpec.describe 'The Manufacturer', type: :feature  do
+  describe "#show page" do
+    it 'displays the manufacturer by :id and its attributes' do
+
+      visit "/manufacturers/#{@jakobs.id}"
+      
+      expect(page).to have_content(@jakobs.name)
+      expect(page).to have_content(@jakobs.headquarters)
+      expect(page).to have_content(@jakobs.elemental?)
+      expect(page).to have_content(@jakobs.equipment_production_total)
+
+      expect(page).to_not have_content(@torgue.headquarters)
+    end
+
+
+    it 'displays the number of weapons per manufacturer' do
+
+      visit "/manufacturers/#{@dahl.id}"
+
+      expect(page).to have_content(@dahl.weapons.count)
+
+      expect(page).to_not have_content(@torgue.weapons.count)
+    end
   end
 end
